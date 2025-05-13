9bb606cf8ca1213e410f99666edbbb0a6ed94f3d
Aoi-Suzuki <aoidon.lifeistech@gmail.com>
2025-05-14 01:36:43 +0900

    トップ画面の作成中。ボタンの動きはつけられるけどボタンとして機能しない

--- Hitsujichan/topViewController.swift
+++ Hitsujichan/topViewController.swift
@@ -8,11 +8,23 @@
 import UIKit
 
 class topViewController: UIViewController {
+    
+    @IBOutlet var startButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+//        self.startButton.center = self.view.center
+        UIView.animate(withDuration: 1.0, delay : 0, options : [.curveEaseIn, .autoreverse, .repeat], animations : {
+            self.startButton.center.y += 10.0
+        }) { _ in
+            self.startButton.center.y -= 10.0
+        }
     }
     
 
