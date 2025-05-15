c770e5fb0be66c871618799d165da2cacd837e4f
Aoi-Suzuki <aoidon.lifeistech@gmail.com>
2025-05-15 16:26:08 +0900

    どうですか泣

--- Hitsujichan/ViewController.swift
+++ Hitsujichan/ViewController.swift
@@ -187,5 +187,5 @@ class TestView: UIView {
             testText.trailingAnchor.constraint(equalTo: self.outline.trailingAnchor, constant: -20),
         ])
     }
-    // テストtest
+    // テストtestどうですか
 }
